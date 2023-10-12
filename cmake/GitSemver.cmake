9840f8e22022ff2e23fd75c7d9798d9715a9f7ac
black-desk <me@black-desk.cn>
2023-10-12 11:10:49 +0800

    build(deps): upgrade GitSemver.cmake
    Signed-off-by: black-desk <me@black-desk.cn>

--- cmake/GitSemver.cmake
+++ cmake/GitSemver.cmake
@@ -1,72 +1,54 @@
-get_property (
-        GITSEMVER_INITIALIZED GLOBAL ""
-        PROPERTY
-                GITSEMVER_INITIALIZED
-        SET
-)
+# SPDX-FileCopyrightText: 2023 Chen Linxuan <me@black-desk.cn>
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+get_property(
+  GITSEMVER_INITIALIZED GLOBAL ""
+  PROPERTY GITSEMVER_INITIALIZED
+  SET)
 if(GITSEMVER_INITIALIZED)
-        return ()
+  return()
 endif()
 
+set_property(GLOBAL PROPERTY GITSEMVER_INITIALIZED true)
+
+function(gitsemver_message)
+  message(STATUS "GitSemver: " ${ARGN})
+endfunction()
+
+message(STATUS "GitSemver: --==Version: v0.1.0==--")
+
+# GitSemver will write the result to varname if it successfully get version
+# string from git repository.
 function(GitSemver varname)
-        if(NOT PROJECT_IS_TOP_LEVEL)
-                return ()
-        endif()
+  if(NOT PROJECT_IS_TOP_LEVEL)
+    return()
+  endif()
 
-        message (
-                STATUS
-                        "GitSemver: Getting version from git repository ..."
-        )
+  gitsemver_message("Getting version from git repository ...")
 
-        find_package (Git REQUIRED)
-        find_program (
-                SED_EXECUTABLE NAMES
-                                       sed REQUIRED
-        )
-        execute_process (
-                COMMAND
-                        ${GIT_EXECUTABLE} describe --tags --long --dirty
-                COMMAND
-                        ${SED_EXECUTABLE} -e
-                        s/-\\\([[:digit:]]\\+\\\)-g/+\\1\\./
-                COMMAND
-                        ${SED_EXECUTABLE} -e s/-dirty\$/\\.dirty/
-                COMMAND
-                        ${SED_EXECUTABLE} -e s/+0\\.[^\\.]\\+\\.\\?/+/
-                COMMAND
-                        ${SED_EXECUTABLE} -e s/^v//
-                COMMAND
-                        ${SED_EXECUTABLE} -e s/+\$//
-                OUTPUT_VARIABLE
-                        ${varname} RESULTS_VARIABLE rets
-                OUTPUT_STRIP_TRAILING_WHITESPACE
-                ERROR_QUIET
-                COMMAND_ECHO
-                STDOUT
-        )
+  find_package(Git REQUIRED)
+  find_program(SED_EXECUTABLE NAMES sed REQUIRED)
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} describe --tags --long --dirty
+    COMMAND ${SED_EXECUTABLE} -e s/-\\\([[:digit:]]\\+\\\)-g/+\\1\\./
+    COMMAND ${SED_EXECUTABLE} -e s/-dirty\$/\\.dirty/
+    COMMAND ${SED_EXECUTABLE} -e s/+0\\.[^\\.]\\+\\.\\?/+/
+    COMMAND ${SED_EXECUTABLE} -e s/^v//
+    COMMAND ${SED_EXECUTABLE} -e s/+\$//
+    OUTPUT_VARIABLE ${varname} RESULTS_VARIABLE rets
+    OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET COMMAND_ECHO STDOUT)
 
-        foreach(ret ${rets})
-                if(NOT
-                   ret
-                   EQUAL
-                   0
-                )
-                        message (
-                                STATUS
-                                        "GitSemver: Failed."
-                        )
-                        return ()
-                endif()
-        endforeach()
+  foreach(ret ${rets})
+    if(NOT ret EQUAL 0)
+      gitsemver_message("Failed.")
+      return()
+    endif()
+  endforeach()
 
-        message (
-                STATUS
-                        "GitSemver: ${${varname}}"
-        )
+  gitsemver_message("${${varname}}")
 
-        set (
-                ${varname}
-                ${${varname}}
-                PARENT_SCOPE
-        )
+  set(${varname}
+      ${${varname}}
+      PARENT_SCOPE)
 endfunction()
