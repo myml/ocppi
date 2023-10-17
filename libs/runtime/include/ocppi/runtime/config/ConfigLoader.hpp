d07db8ea9cccd8fc5057f1795327d32ed986e51d
black-desk <me@black-desk.cn>
2023-10-17 14:05:04 +0800

    chore(runtime): add missing nodiscard to ConfigLoader::load
    Signed-off-by: black-desk <me@black-desk.cn>

--- libs/runtime/include/ocppi/runtime/config/ConfigLoader.hpp
+++ libs/runtime/include/ocppi/runtime/config/ConfigLoader.hpp
@@ -14,6 +14,7 @@ namespace ocppi::runtime::config
 {
 
 struct ConfigLoader {
+        [[nodiscard]]
         auto load(std::istream &in) const noexcept
                 -> tl::expected<types::Config, std::exception_ptr>;
 };
